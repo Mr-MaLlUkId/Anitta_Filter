25f7529fc8cd42960df1263b9c524732af5b5381
MrMalluKiD <mrmallukid@gmail.com>
2024-11-17 23:56:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
-TIMEZONE = (os.environ.get("TIMEZONE", "Asia/Kolkata"))
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
@@ -14,14 +13,30 @@ from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL
 from utils import temp
-from plugins import web_server
-from aiohttp import web
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from Script import script
+
+import schedule
+import asyncio
+from datetime import date, datetime, timedelta
+import pytz
+
+from plugins.webcode import bot_run
+from os import environ
+from aiohttp import web as webserver
+
+PORT_CODE = environ.get("PORT", "8080")
+
+
+
+
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=SESSION,
+            name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -43,20 +58,71 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
-        if LOG_CHANNEL
-            try:
-                await self.send_message(LOG_CHANNEL, text=f"<b>{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{date}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")                      
-            except Unauthorized:
-                LOGGER.warning("Bot isn't able to send message to LOG_CHANNEL")
-            except BadRequest as e:
-                LOGGER.error(e)                         
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT)#RESTART SND IN LOG_CHANNEL
+        print("Mantis own Bot</>")
+
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+        
+        client = webserver.AppRunner(await bot_run())
+        await client.setup()
+        bind_address = "0.0.0.0"
+        await webserver.TCPSite(client, bind_address,
+        PORT_CODE).start()
+                          
+        # Schedule the restart every 24 hours
+        schedule.every(24).hours.do(lambda: asyncio.create_task(self.restart()))
+
+        #----------- Start the scheduler in a background task
+        asyncio.create_task(self.run_scheduler())
+  
+    async def restart(self):
+        logging.info(" Is it 12pm!! Bot is restarting...")
+        await self.stop()
+        await self.start()  # Restart the bot
+
+    async def run_scheduler(self):
+        while True:
+            schedule.run_pending()
+            await asyncio.sleep(1)  # Prevent busy-waiting--------------
+        
 
     async def stop(self, *args):
         await super().stop()
-        me = await self.get_me()
-        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
-
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
+        logging.info("Bot stopped. Bye.")
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                for message in app.iter_messages("pyrogram", 1, 15000):
+                    print(message.text)
+        """
         current = offset
         while True:
             new_diff = min(200, limit - current)
@@ -68,6 +134,5 @@ class Bot(Client):
                 current += 1
 
 
-        
 app = Bot()
 app.run()
