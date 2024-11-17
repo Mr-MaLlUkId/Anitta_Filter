9ae92fb671220b0a180a5980c9ae874de488e5b6
MrMalluKiD <mrmallukid@gmail.com>
2024-11-18 01:40:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,7 +101,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode=enums.ParseMode.HTML
         )
         return
     data = message.command[1]
