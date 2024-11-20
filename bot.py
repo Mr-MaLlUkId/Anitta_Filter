d74b7165569e0fc1ad07ddd93194ec970cf502ef
MrMalluKiD <mrmallukid@gmail.com>
2024-11-20 11:10:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,6 +47,9 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
+        curr = datetime.now(timezone(TIMEZONE))
+        date = curr.strftime('%d %B, %Y')
+        time = curr.strftime('%I:%M:%S %p')
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
