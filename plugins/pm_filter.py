f80d9f40d354c5a6322cda8ddbff76e62f4776e2
MrMalluKiD <mrmallukid@gmail.com>
2024-11-24 03:54:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -477,10 +477,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto("https://envs.sh/KjC.jpg", has_spoiler=True)
+            InputMediaPhoto("https://envs.sh/KjC.jpg")
         )
         await query.message.edit_text(
-            text=script.HELP_TXT,
+            text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
