a0ff33a6363822fc18503bd82a05365cbba14c7c
MrMalluKiD <mrmallukid@gmail.com>
2024-11-26 03:45:08 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -55,7 +55,7 @@ async def save_group(bot, message):
                         pass
                 temp.MELCOW['welcome'] = await message.reply_photo(
                                                  photo=(WELCOM_PIC),
-                                                 caption=(WELCOM_TEXT.format(u.mention, message.chat.title)),
+                                                 caption=(WELCOM_TEXT.format(user=u.mention, chat=message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
                                                                            InlineKeyboardButton('⚠️Movie Updates⚠️', url='https://t.me/cinema_flix_updates')
