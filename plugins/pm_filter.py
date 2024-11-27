2f9ac1dfee217224133a95315bf18659353da48a
MrMalluKiD <mrmallukid@gmail.com>
2024-11-27 21:46:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🗃 [{get_size(file.file_size)}]  {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🗃 [{get_size(file.file_size)}] ▷ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -713,7 +713,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🗃 [{get_size(file.file_size)}]  {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"🗃 [{get_size(file.file_size)}] ▷ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
